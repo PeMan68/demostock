a4ab87c96d0c90153726fd51364c64811775b47a
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2019-07-23 15:59:49 +0200

    created migration for stock

---
+++ database/migrations/2019_07_23_134602_create_stocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->string('item',50);
+			$table->string('item_description',50);
+			$table->mediumText('notes');
+			$table->float('listprice')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
